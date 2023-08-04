0c636a7597d92dde43c096b173468bd358eb6a96
Rainer Simon <rainer@rainersimon.io>
2023-08-04 10:42:41 +0200

    shadcn setup

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -11,10 +12,15 @@ export default defineConfig(({ command, mode }) => ({
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ["react"],
-          "react-dom": ["react-dom"],
+          react: ['react'],
+          'react-dom': ['react-dom'],
         }
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   }
 }));
